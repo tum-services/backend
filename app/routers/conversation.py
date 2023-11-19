12e23edd04449224ba02491e92d19f0c2178e123
Rico <#finkbeinerrico@gmail.com>
2023-11-19 06:32:13 +0100

    fix

--- app/routers/conversation.py
+++ app/routers/conversation.py
@@ -78,7 +78,7 @@ async def new_conv(conv: ConversationInput) -> dict:
 @router.get("/")
 async def get_convs():
     cs = ConversationSummary.collection.fetch()
-    return [c.to_dict() for c in cs].sort(key=lambda x: x["date"])
+    return [c.to_dict() for c in cs].sort(key=lambda x: x["date"], reverse=True)
 
 
 @router.delete("/{id}")
